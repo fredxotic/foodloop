46120efd0316e7bae6c1c1579ff9487ad3e89bc0
Fred <charlesfred285@gmail.com>
2025-09-24 10:45:16 +0300

    Launching the site

--- foodloop/wsgi.py
+++ foodloop/wsgi.py
@@ -1,16 +1,21 @@
-"""
-WSGI config for foodloop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
 
 import os
+import sys
 
+# Add your project directory to the Python path
+path = '/home/fredxotic/foodloop'  # UPDATE WITH YOUR USERNAME AND PATH
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Set environment variables
+os.environ['DJANGO_SETTINGS_MODULE'] = 'foodloop.settings'
+
+# Import and run Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodloop.settings")
-
 application = get_wsgi_application()
