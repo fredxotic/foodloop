80fddaf6328e7d06f21e7cfed11bed0f27915ab8
fredxotic <charlesfred285@gmail.com>
2025-12-01 19:08:23 +0300

    better build script

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ pip install -r requirements.txt
 # Convert static assets
 python manage.py collectstatic --no-input
 
+# Create database migrations
+python manage.py makemigrations
+
 # Apply database migrations
 python manage.py migrate
