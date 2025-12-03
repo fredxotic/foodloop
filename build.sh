1d3e71e33df7030f8a80a115f09f9221fb543a29
fredxotic <charlesfred285@gmail.com>
2025-12-03 22:13:15 +0300

    Trusting my Goddamn insticts

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ python manage.py collectstatic --no-input
 
 echo "=== Starting database setup ==="
 
+# Apply any pending migrations
+python manage.py makemigrations core
+
 # Run all migrations normally
-python manage.py migrate --noinput
+python manage.py migrate core
 
 echo "=== Database setup completed ==="
