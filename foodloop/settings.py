25e5dc5ef570a101b58267efcc69e46a7d4c949d
fredxotic <charlesfred285@gmail.com>
2025-12-04 21:09:09 +0300

    Added emoji support for MySQL

--- foodloop/settings.py
+++ foodloop/settings.py
@@ -121,8 +121,13 @@ else:
             'PASSWORD': config('DB_PASSWORD', default=''),
             'HOST': config('DB_HOST', default='localhost'),
             'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
                 'charset': 'utf8mb4',
+                # Set charset and collation for emoji support
+                'init_command': (
+                    "SET sql_mode='STRICT_TRANS_TABLES', "
+                    "character_set_connection=utf8mb4, "
+                    "collation_connection=utf8mb4_unicode_ci"
+                ),
             },
         }
     }
